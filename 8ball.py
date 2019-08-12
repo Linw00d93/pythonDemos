6ecad046edd1a6e26cbe0011bb3951d2cc880fd7
Linwood Robinson <RobinsonLinwoodE@JohnDeere.com>
2019-08-12 16:00:31 -0500

    Update 8ball.py

--- 8ball.py
+++ 8ball.py
@@ -7,85 +7,85 @@ import Time
 randomNumber = random.randint(1,20)
 alive = True
 print "Welcome to the Magic 8 ball"
-    while alive == True
-        print "Do you have a question for the Magic 8"
-        raw = raw_input()
-        userInput = raw.lower()
-        if userInput == "yes" or userInput =='y':
-            print "What is your question"
-            userQuestion = raw_input()
-            print "Let me think about that"
-            time.sleep(1)
-            print(.)
-            time.sleep(2)
-            print(..)
-            time.sleep(3)
-            print(...)
-            if randomNumber = 1
-            print "As I see it, yes."
-            break
-            elif randomNumber = 2
-            print "Ask again later."
-            break
-            elif randomNumber = 3
-            print "Better not tell you now."
-            break
-            elif randomNumber = 4
-            print "Cannot predict now."
-            break
-            elif randomNumber = 5
-            print "Concentrate and ask again."
-            break
-            elif randomNumber = 6
-            print "Don’t count on it."
-            break
-            elif randomNumber = 7
-            print "It is certain."
-            break
-            elif randomNumber = 8
-            print "It is decidedly so."
-            break
-            elif randomNumber = 9
-            print "Most likely."
-            break
-            elif randomNumber = 10
-            print "My reply is no."
-            break
-            elif randomNumber = 11
-            print "My sources say no."
-            break
-            elif randomNumber = 12
-            print "Outlook not so good."
-            break
-            elif randomNumber = 13
-            print "Outlook good."
-            break
-            elif randomNumber = 14
-            print "Reply hazy, try again."
-            break
-            elif randomNumber = 15
-            print "Signs point to yes."
-            break
-            elif randomNumber = 16
-            print "Very doubtful."
-            break
-            elif randomNumber = 17
-            print "Without a doubt."
-            break
-            elif randomNumber = 18
-            print "Yes."
-            break
-            elif randomNumber = 19
-            print "Yes – definitely."
-            break
-            elif randomNumber = 20
-            print "You may rely on it."
-            break
-        elif userInput == "no" or userInput == "n":
-            print "Bye then"
-            exit()
-        else:
-            print "ERROR error Error"
+continue
+print "Do you have a question for the Magic 8"
+raw = raw_input()
+userInput = raw.lower()
+if userInput == "yes" or userInput =='y':
+    print "What is your question"
+    userQuestion = raw_input()
+    print "Let me think about that"
+    time.sleep(1)
+    print "."
+    time.sleep(2)
+    print ".."
+    time.sleep(3)
+    print "..."
+    if randomNumber == 1:
+    print "As I see it, yes."
+    break
+    elif randomNumber == 2:
+    print "Ask again later."
+    break
+    elif randomNumber == 3:
+    print "Better not tell you now."
+    break
+    elif randomNumber == 4:
+    print "Cannot predict now."
+    break
+    elif randomNumber == 5
+    print "Concentrate and ask again."
+    break
+    elif randomNumber == 6
+    print "Don’t count on it."
+    break
+    elif randomNumber == 7
+    print "It is certain."
+    break
+    elif randomNumber == 8
+    print "It is decidedly so."
+    break
+    elif randomNumber == 9
+    print "Most likely."
+    break
+    elif randomNumber == 10
+    print "My reply is no."
+    break
+    elif randomNumber == 11
+    print "My sources say no."
+    break
+    elif randomNumber == 12
+    print "Outlook not so good."
+    break
+    elif randomNumber == 13
+    print "Outlook good."
+    break
+    elif randomNumber == 14
+    print "Reply hazy, try again."
+    break
+    elif randomNumber == 15
+    print "Signs point to yes."
+    break
+    elif randomNumber == 16
+    print "Very doubtful."
+    break
+    elif randomNumber == 17:
+    print "Without a doubt."
+    break
+    elif randomNumber == 18:
+    print "Yes."
+    break
+    elif randomNumber == 19:
+    print "Yes – definitely."
+    break
+    elif randomNumber == 20:
+    print "You may rely on it."
+    break
+elif userInput == "no" or userInput == "n":
+    print "Bye then"
+    exit()
+else:
+    print "ERROR error Error"
 
 
 #    ,dP9CGG88@b,
