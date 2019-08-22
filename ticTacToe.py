fdd0c25c3cb8198b6cbe9945b18098619d6ba9af
Linwood <Linwood93@gmail.com>
2019-08-21 21:11:50 -0500

    Update ticTacToe.py

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,3 +1,7 @@
+import random
+winConditionX = 'XXX'
+winConditionO = 'OOO'
+winCondition = False
 userInput = 0
 randomNumber = 0
 dash = '|'
@@ -15,20 +19,193 @@ line1 = square1 + dash + square2 + dash + square3
 lineBetween = '-----'
 line2 = square4 + dash + square5 + dash + square6
 line3 = square7 + dash + square8 + dash + square9
-print line1
-print lineBetween
-print line2
-print lineBetween
-print line3
 playing = True
 
+win123 = ' '
+win159 = ' '
+win147 = ' '
+win258 = ' '
+win357 = ' '
+win369 = ' '
+win456 = ' '
+win789 = ' '
 
 
 while playing == True:
+    win123 = square1 + square2 + square3
+    win159 = square1 + square5 + square9
+    win149 = square1 + square4 + square9
+    win258 = square2 + square5 + square8
+    win357 = square3 + square5 + square7
+    win369 = square3 + square6 + square9
+    win456 = square4 + square5 + square6
+    win789 = square7 + square8 + square9
+    if win123 == winConditionO:
+        print'The AI Won'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win123 == winConditionX:
+        print'You beat the AI'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win159 == winConditionO:
+        print'The AI Won'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win159 == winConditionX:
+        print'You beat the AI'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win147 == winConditionO:
+        print'The AI Won'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win147 == winConditionX:
+        print'You beat the AI'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win258 == winConditionO:
+        print'The AI Won'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win258 == winConditionX:
+        print'You beat the AI'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win357 == winConditionO:
+        print'The AI Won'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win357 == winConditionX:
+        print'You beat the AI'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win369 == winConditionO:
+        print'The AI Won'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win369 == winConditionX:
+        print'You beat the AI'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win456 == winConditionO:
+        print'The AI Won'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win456 == winConditionX:
+        print'You beat the AI'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win789 == winConditionO:
+        print'The AI Won'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+    elif win789 == winConditionX:
+        print'You beat the AI'
+        print square1 + dash + square2 + dash + square3
+        print lineBetween
+        print square4 + dash + square5 + dash + square6
+        print lineBetween
+        print square7 + dash + square8 + dash + square9
+        print'Good bye'
+        playing = False
+        SystemExit()
+
 
     userInput = raw_input('Would you like to play Tic Tac Toe ')
     if userInput == 'yes' or userInput == 'y':
-        iconInput = input('What Position would you like to put your icon')
+        iconInput = input('What position would you like to put your icon; Options are 1-9 of the board ')
+
         if iconInput == 1:
             if square1 == 'X' or square1 == 'O':
                 print'That square has already been used, try again'
@@ -40,15 +217,15 @@ while playing == True:
                 print lineBetween
                 print square7 + dash + square8 + dash + square9
         elif iconInput == 2:
-                if square2 == 'X' or square2 == 'O':
-                    print'That square has already been used, try again'
-                else:
-                    square2 = 'X'
-                    print square1 + dash + square2 + dash + square3
-                    print lineBetween
-                    print square4 + dash + square5 + dash + square6
-                    print lineBetween
-                    print square7 + dash + square8 + dash + square9
+            if square2 == 'X' or square2 == 'O':
+                print'That square has already been used, try again'
+            else:
+                square2 = 'X'
+                print square1 + dash + square2 + dash + square3
+                print lineBetween
+                print square4 + dash + square5 + dash + square6
+                print lineBetween
+                print square7 + dash + square8 + dash + square9
         elif iconInput == 3:
             if square3 == 'X' or square3 == 'O':
                 print'That square has already been used, try again'
@@ -121,26 +298,106 @@ while playing == True:
                 print square7 + dash + square8 + dash + square9
         else:
             print'Error'
+
+    #AI turn
+        print'AI Turn'
+        randomNumber = random.randint(1,9)
+        if randomNumber == 1:
+            if square1 == 'X' or square1 == 'O':
+                print'That square has already been used, try again'
+            else:
+                square1 = 'O'
+                print square1 + dash + square2 + dash + square3
+                print lineBetween
+                print square4 + dash + square5 + dash + square6
+                print lineBetween
+                print square7 + dash + square8 + dash + square9
+        elif randomNumber == 2:
+            if square2 == 'X' or square2 == 'O':
+                print'That square has already been used, try again'
+            else:
+                square2 = 'O'
+                print square1 + dash + square2 + dash + square3
+                print lineBetween
+                print square4 + dash + square5 + dash + square6
+                print lineBetween
+                print square7 + dash + square8 + dash + square9
+        elif randomNumber == 3:
+            if square3 == 'X' or square3 == 'O':
+                print'That square has already been used, try again'
+            else:
+                square3 = 'O'
+                print square1 + dash + square2 + dash + square3
+                print lineBetween
+                print square4 + dash + square5 + dash + square6
+                print lineBetween
+                print square7 + dash + square8 + dash + square9
+        elif randomNumber == 4:
+            if square4 == 'X' or square4 =='O':
+                print'That square has already been used, try again'
+            else:
+                square4 = 'O'
+                print square1 + dash + square2 + dash + square3
+                print lineBetween
+                print square4 + dash + square5 + dash + square6
+                print lineBetween
+                print square7 + dash + square8 + dash + square9
+        elif randomNumber == 5:
+            if square5 == 'X' or square5 == 'O':
+                print'That square has already been used, try again'
+            else:
+                square5 = 'O'
+                print square1 + dash + square2 + dash + square3
+                print lineBetween
+                print square4 + dash + square5 + dash + square6
+                print lineBetween
+                print square7 + dash + square8 + dash + square9
+        elif randomNumber == 6:
+            if square6 == 'X' or square6 == 'O':
+                print'That square has already been used, try again'
+            else:
+                square6 = 'O'
+                print square1 + dash + square2 + dash + square3
+                print lineBetween
+                print square4 + dash + square5 + dash + square6
+                print lineBetween
+                print square7 + dash + square8 + dash + square9
+        elif randomNumber == 7:
+            if square7 == 'X' or square7 == 'O':
+                print'That square has already been used, try again'
+            else:
+                square7 = 'O'
+                print square1 + dash + square2 + dash + square3
+                print lineBetween
+                print square4 + dash + square5 + dash + square6
+                print lineBetween
+                print square7 + dash + square8 + dash + square9
+        elif randomNumber == 8:
+            if square8 == 'X' or square8 == 'O':
+                print'That square has already been used, try again'
+            else:
+                square8 = 'O'
+                print square1 + dash + square2 + dash + square3
+                print lineBetween
+                print square4 + dash + square5 + dash + square6
+                print lineBetween
+                print square7 + dash + square8 + dash + square9
+        elif randomNumber == 9:
+            if square9 == 'X' or square9 == 'O':
+                print'That square has already been used, try again'
+            else:
+                square9 = 'O'
+                print square1 + dash + square2 + dash + square3
+                print lineBetween
+                print square4 + dash + square5 + dash + square6
+                print lineBetween
+                print square7 + dash + square8 + dash + square9
+        else:
+            print'Error'
+
     elif userInput =='no' or userInput == 'n':
         print'Good bye'
         playing = False
         SystemExit()
 else:
     print 'Error'
-
-
-
-
-
-"""
-if userInput = 1
-elif userInput = 2
-elif userInput = 3
-elif
-elif
-elif
-elif
-elif
-elif
-else
-"""
