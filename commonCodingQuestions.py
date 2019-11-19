90fbc245919c737cb74dab3832e38be19f167835
Linw00d93 <linwood93@gmail.com>
2019-11-18 21:43:54 -0600

    3

--- commonCodingQuestions.py
+++ commonCodingQuestions.py
@@ -1,7 +1,8 @@
 #common coding questions
 import copy
+import numpy
 arrayText = [ 1,3,45,76,124,523,63,12,431,-9,67,34,124,768,97,65,423,135,876,96,99,22,4,55,64,7,39,9,23,12,21,43,65,79,98,72,5,9]
-x = 0
+"""x = 0
 y = 0
 numberFound = False
 
@@ -21,7 +22,7 @@ else:
     txt1 = wordtext + ' is not a Palindrome'
     print (txt1)
 
-"""
+
 while numberFound == False:
 
     for i in range(len(arrayText)):
@@ -37,3 +38,20 @@ while numberFound == False:
     x = 0
     y = y + 1
 """
+
+arrayText1 = [1,2,3,4]
+product =1
+num2 = 0
+Long = len(arrayText1)
+for items in range(Long):
+    num2 = arrayText1.pop(items)
+    print ("arrayText")
+    print(arrayText1)
+    print("num2")
+    print (num2)
+    #arrayText.insert(items,num2)
+    #print (arrayText)
+    for x in range(Long):
+
+    arrayText.insert(items,num2)
+    print (arrayText1)
