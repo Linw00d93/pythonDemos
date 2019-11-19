bae1e425e907d9e6805d9104c68c4794fff44d28
Linw00d93 <42758858+Linw00d93@users.noreply.github.com>
2019-11-19 15:42:20 -0600

    Update commonCodingQuestions.py

--- commonCodingQuestions.py
+++ commonCodingQuestions.py
@@ -108,3 +108,7 @@ for x in range(min1,scale):
         print(givenArray)
         exit()
 """
+
+#Daily Coding Problem
+#Given the mapping a = 1, b = 2, ... z = 26, 
+#and an encoded message, count the number of ways it can be decoded.
